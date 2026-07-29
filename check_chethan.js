a3abb47c8bf8de311740474c2e3d905a806e829f
saranthecoder <aspiringmind05@gmail.com>
2026-07-29 10:01:40 +0530

    update 12

--- check_chethan.js
+++ check_chethan.js
@@ -5,6 +5,7 @@ const Submission = require('./models/Submission');
 const Grade = require('./models/Grade');
 const Task = require('./models/Task');
 
+
 async function run() {
   await mongoose.connect(process.env.MONGODB_URI);
   console.log("Connected to DB");
